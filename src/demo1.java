cbd79c4fd5e5158223380e6bb28ab6f3d16e926d
Ishika Dutta <ishikadutta@Ishikas-MacBook-Pro.local>
2021-02-16 17:46:11 +0530

    third commit

--- src/demo1.java
+++ src/demo1.java
@@ -1,5 +1,9 @@
 public class demo1 {
     public static void main(String ar[]){
         System.out.println("Helloe");
+        int c,d;
+        c=10;
+        d=2;
+        System.out.println(c-d);
     }
 }
